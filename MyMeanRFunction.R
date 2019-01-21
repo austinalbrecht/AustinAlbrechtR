3217e5cf975adb1fa15fdc14afb1c02134410442
Austin Albrecht <austin.albrecht0@gmail.com>
2019-01-21 02:13:16 -0500

    Create MyMeanRFunction.R

---
+++ MyMeanRFunction.R
@@ -0,0 +1,6 @@
+assignment2<- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+MyMean<- function(X) { return(sum(X)/length(X))}
+MyMean(assignment2)
+mean(assignment2)
+#Function MyMean works the same as R built in function mean() so the MyMean format 
+#works in this case.
